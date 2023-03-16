0dfcf588e372553394d8f52c14c950cfb01dd3fc
mavine4512 <mavineoyugi@gmail.com>
2023-03-16 03:16:18 -0700

    Hello, puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts function
+ * Return: always 0 (Succes)
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
