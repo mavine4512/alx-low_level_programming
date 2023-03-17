25a8a22306e26dcb50d6e7e70373def588e97058
mavine4512 <mavineoyugi@gmail.com>
2023-03-17 09:44:53 -0700

    fixed numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		printf(n);
+	}
+	printf('\n');
+	return (0);
+}
