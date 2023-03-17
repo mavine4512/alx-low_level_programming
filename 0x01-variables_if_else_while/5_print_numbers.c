dde541950b0c0a969e7e7b61ed4d1e0f4e2a342e
mavine4512 <mavineoyugi@gmail.com>
2023-03-17 09:06:31 -0700

    numbers

--- 0x01-variables_if_else_while/5_print_numbers.c
+++ 0x01-variables_if_else_while/5_print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base
- * 10 starting from 0 .
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int num[10] = "0123456789"
 	int i;
 
 	for (i = 0; i < 10; i++)
-		printf("%d", i);
+		printf("%d", num[i]);
 	printf("\n");
 	return (0);
 }
