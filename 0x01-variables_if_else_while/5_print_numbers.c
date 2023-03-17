6b765cdaf7b5630c573d0151c9f3a0dc290c80fe
mavine4512 <mavineoyugi@gmail.com>
2023-03-17 08:23:47 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5_print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers base ten from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
