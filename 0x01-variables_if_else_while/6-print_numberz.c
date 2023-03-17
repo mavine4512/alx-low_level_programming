583af0409a9a33cc3c071f0fd3bdac10c9977acc
mavine4512 <mavineoyugi@gmail.com>
2023-03-17 09:24:21 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,19 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
+ * using putchar
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int num[10] = "0123456789"
 	int n;
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
+		putchar(num[n]);
 	}
 	putchar('\n');
 	return (0);
 }
-
