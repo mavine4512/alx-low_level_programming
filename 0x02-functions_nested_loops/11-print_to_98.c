d028c26d7774b0d3a4a829832d05e6620c2e4db7
mavine4512 <mavineoyugi@gmail.com>
2023-03-21 13:14:19 -0700

    list numbers fro zero to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: number
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 98; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
