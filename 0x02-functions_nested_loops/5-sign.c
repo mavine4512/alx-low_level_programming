2f4f09fb6133f749cb2a8c2e416973d185a5fc9b
mavine4512 <mavineoyugi@gmail.com>
2023-03-21 08:20:13 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check if character is lowercase
+ * @n: Checks if the character 'n' is grater than or equal to zero
+ * Return: 1 if n is greater then zero,-1 id is less than zero
+ * 0  otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
