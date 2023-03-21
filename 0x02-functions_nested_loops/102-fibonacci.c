9ca718cd67a9e5431ff13b779d31e90a0891be2c
mavine4512 <mavineoyugi@gmail.com>
2023-03-21 15:42:33 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%d, %d", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+
+}
