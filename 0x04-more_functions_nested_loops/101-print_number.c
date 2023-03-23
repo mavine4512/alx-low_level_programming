a639dd4e8cd0d186b0e074eb79ef34e1aa19fba2
mavine4512 <mavineoyugi@gmail.com>
2023-03-23 11:03:21 -0700

    print interger

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number- a function that prints an integer.
+ * @n: integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	_putchar(n);
+	_putchar('\n');
+
+	return (0);
+}
