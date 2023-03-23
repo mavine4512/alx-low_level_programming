53856e0ed77e47db5c466a3ad2e5b0601b728e5d
mavine4512 <mavineoyugi@gmail.com>
2023-03-23 09:33:29 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i / 15) == 0)
+		if ((i % 15) == 0)
 		{
 			printf(" FizzBuzz");
 		}
-		else if ((i / 3) == 0)
+		else if ((i % 3) == 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((i / 5) == 0)
+		else if ((i % 5) == 0)
 		{
 			printf(" Buzz");
 		}
