7a4684e221b4797c6888dfac4e065a0bb8674b62
mavine4512 <mavineoyugi@gmail.com>
2023-03-23 03:56:35 -0700

    fixed numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - loop through 0 to 9
- *
- * Return: void
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
