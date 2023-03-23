efd99c9b500903b531b6950c8ff9eb57b2315961
mavine4512 <mavineoyugi@gmail.com>
2023-03-23 10:12:32 -0700

    prints the numbers from 1 to 100, followed by a new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,36 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - prints the number from 1 - 100, but for mult of 3 & 5 print Fizz Buzz
  *
  * Return: Always 0 (Success)
- */
+**/
+
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1 ; i < 100 ; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf(" %d", i);
+		printf("%d ", i);
 		}
 	}
+
+	printf("Buzz");
 	printf("\n");
 
 	return (0);
