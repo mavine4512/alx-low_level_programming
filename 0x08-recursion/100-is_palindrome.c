e72aa1e2e0e9ee21ed1545f1cfddf82756eeadc5
mavine4512 <mavineoyugi@gmail.com>
2023-04-05 14:43:40 -0700

    if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 int _strlen_recursion(char *s);
 int help_chack_pal(char *s, int i, int len);
@@ -25,7 +24,7 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (1);
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
