b2a85dcb72f2e9232b550a2687d367b7a258cb2c
mavine4512 <mavineoyugi@gmail.com>
2023-04-13 07:14:39 -0700

    Function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,12 +12,12 @@ int *array_range(int min, int max)
 	int *ptr, i; /*variable of an interge and a pointer*/
 
 	if (min > max)
-		return (NULL); /*check if a minimum is greater then maximum then return NULL*/
+		return (NULL); /*check if minimum is greater then maximum then return NULL*/
 
-	ptr = malloc((max - min + 1) * sizeof(*ptr));/* Allocate a memory using malloc function to create a block of memory that is large enough to hold max - min value plus 1 of type pointer*/
+	ptr = malloc((max - min + 1) * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);/*check if pointer is equal to NULL then return NULL*/
 	for (i = 0; min <= max; i++, min++)
-		ptr[i] = min; /* loop condition the value of min is assigned to ptr[i], and then both i and min are incremented by 1 using the i++, min++ expression.*/
+		ptr[i] = min;
 	return (ptr);
 }
