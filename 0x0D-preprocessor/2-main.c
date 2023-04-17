bb72c7f599744fceb944a9be19c492dc9186e6b4
mavine4512 <mavineoyugi@gmail.com>
2023-04-17 04:44:13 -0700

    fixes C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print file name with newline
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+The C preprocessor is a tool that performs textual 
+transformations on a C source file before it is compiled.
+
