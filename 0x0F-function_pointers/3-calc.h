87a55e7f9b475503cb80f261b264895adead3bf9
mavine4512 <mavineoyugi@gmail.com>
2023-04-19 14:51:38 -0700

    fixed issues

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -16,7 +16,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_div(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,9 +19,9 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
