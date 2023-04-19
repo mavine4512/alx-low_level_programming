607925e33d7bc63b083fffb9eecfaf8d40faed01
mavine4512 <mavineoyugi@gmail.com>
2023-04-19 13:19:48 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,15 +16,14 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i;
-	
+
 	i = 0;
-	
+
 	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	
 	return (NULL);
 }
