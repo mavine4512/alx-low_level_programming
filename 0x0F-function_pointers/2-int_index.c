d0f36bd9dd8921566a5ab38b0032b9b09aa76575
mavine4512 <mavineoyugi@gmail.com>
2023-04-19 07:58:52 -0700

    search for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of first element that matches with cmp or -1 for none found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	for (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
