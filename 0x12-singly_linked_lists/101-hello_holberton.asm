41e7f3987e2299f0c4c6b8f6c14d16f78edcef5f
“mavine4512” <mavineoyugi@gmail.com>
2023-04-27 05:24:45 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,10 @@
-	global    main
-	extern    printf
+          global    main
+          extern    printf
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
+
