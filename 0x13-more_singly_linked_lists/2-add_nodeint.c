357e87a7a19290b87976401a0cb47228facf20e7
“mavine4512” <mavineoyugi@gmail.com>
2023-04-29 04:16:11 -0700

    recursive function

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,19 +10,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	if (!head)
+	new = malloc(sizeof(listint_t));
+	if (!new)
 		return (NULL);
 
-	if (!(*head))
-	{
-		new = malloc(sizeof(listint_t));
-		if (!new)
-			return (NULL);
-		new->n = n;
-		new->next = NULL;
-		*head = new;
-		return (new);
-	}
+	new->n  = n;
+	new->next = *head;
+	*head = new;
 
-	return (add_nodeint(&((*head)->next), n));
+	return (new);
 }
