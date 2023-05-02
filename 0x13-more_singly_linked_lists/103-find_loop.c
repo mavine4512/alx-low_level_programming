5e3fe06d37f8cb5b08c546e79e67bf5290f84bc4
“mavine4512” <mavineoyugi@gmail.com>
2023-05-02 14:25:26 -0700

    fixed loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * ind_listint_loop - Finds a loop in a linked list
+ * find_listint_loop - Finds a loop in a linked list
  * @head: A pointer to the head node of the list
  * Return: if a loop is found, a pointer to the node
  * where the loop starts, if there is no loop - NULL
@@ -13,20 +13,20 @@ listint_t *find_listint_loop(listint_t *head)
 	if (!head)
 		return (NULL);
 
-	do {
-		if (fast == NULL || fast->next == NULL)
-			return (NULL);
-
-		slow = slow->next;
-		fast = fast->next->next;
-	} while (slow != fast);
-
-	slow = head;
-	while (slow != fast)
+	for (; slow && fast && fast->next; slow = slow->next,
+			fast = fast->next->next)
 	{
-		slow = slow->next;
-		fast = fast->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
 	}
 
-	return (slow);
+	return (NULL);
 }
