a4383c001082c6f3d943f9c0e6415adec56dd102
“mavine4512” <mavineoyugi@gmail.com>
2023-05-08 04:09:50 -0700

    fixed issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = wite(o, text_content, len);
+	w = write(o, text_content, len);
 
 	if (0 == -1 || w == -1)
 		return (-1);
