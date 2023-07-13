9c20dcd8058d654c23d71d6f230940e37244910d
“mavine4512” <mavineoyugi@gmail.com>
2023-07-13 14:20:40 -0700

    fixed betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -30,12 +30,12 @@ void check_elf(unsigned char *e_ident)
 	for (index = 0; index < 4; index++)
 	{
 		if (e_ident[index] != 127 &&
-				e_ident[index] != 'E' &&
-				e_ident[index] != 'L' &&
-				e_ident[index] != 'F')
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
 		{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
+		    dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		    exit(98);
 		}
 	}
 }
